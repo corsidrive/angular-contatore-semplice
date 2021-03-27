eec0a025b4e70900e1b8e8e50711d107c26cd6fb
corsidrive <corsidrive@gmail.com>
2021-03-27 17:06:41 +0100

    4 files modified

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # angular-contatore-semplice
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-contatore-semplice)
+
+
+## 01 - counter component
+
+Semplice contatore che gestisce il suo stato interno
+
+## 02 - passing data to component
+
+Inizializzare più contatori con diverse caratteristiche impostate dall'esterno.
+
+- valore iniziale del contatore
+- valore massimo
+- valore minimo
+- step di incremento
+
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "counter",
@@ -6,17 +6,25 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./counter.component.css"]
 })
 export class CounterComponent implements OnInit {
+  @Input()
   label: string = "contatore";
+  @Input()
+  max: number;
+
+  @Input()
   count: number = 0;
+  test: number = 0;
   constructor() {}
 
-  increment(){
-    this.count++;
+  increment() {
+    if (this.max !== undefined) {
+      this.count = this.count < this.max ? ++this.count : this.max;
+    } else {
+      ++this.count;
+    }
   }
 
-  decrement(){
-    this.count--;
-  }
+  decrement() {}
 
   ngOnInit() {}
 }
